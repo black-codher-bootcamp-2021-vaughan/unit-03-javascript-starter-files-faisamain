93a3f924de9f5e2ea1add794f35bb118a9d64e89
faisamain <faisamain@hotmail.com>
2021-11-29 20:18:26 +0000

    var practice

--- session-2/index.js
+++ session-2/index.js
@@ -1,2 +1,6 @@
-// This is a comment in JavaScript
-// You can see where this file is connected to your HTML in the index.html on line 14
+// This is a comment in Javascript
+// You can see where this file is connected to your HTML in the index.html
+
+console.log("Hello everyone")
+console.log(document.querySelector('.about').innerHTML)
+console.log(document.querySelectorAll('.about'))
