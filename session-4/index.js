76977f2fcd6f2b4705fe06ef750ab5a215d22297
faisamain <faisamain@hotmail.com>
2021-12-08 18:45:31 +0000

    session tasks

--- session-4/index.js
+++ session-4/index.js
@@ -3,10 +3,55 @@
 
 
 
+const personA = {
+Name: "Karen",
+Age: "21",
+Location: "NewYork",
+Likes: "Food"
+
+};
+
+const personB = {
+    Name: "Julia",
+    Age: "16",
+    Location: "London",
+    Likes: "IceSkating",
+    
+    };
+
+    const personC = {
+        Name: "Gina",
+        Age: "29",
+        Location: "Bristol",
+        Likes: "Travelling",
+        
+        };
+
+        const personD = {
+            Name: "Asma",
+            Age: "34",
+            Location: "NewCastle",
+            Likes: "Coding"
+            
+            };
+
+//*console.log(personA);
+
+function biography(person) {
+    return "Hi, " + " my name is " + person.Name + ". I am " + person.Age + " years old. " + " I live in " + person.Location + " ,and I like " + person.Likes;
+}
 
 
+console.log(biography(personA))
 
+const person = 24;
+const drivingage = 18;
 
+const isdrivingage = person > drivingage;
+
+console.log('Is person of Driving Age?' + isdrivingage);
+
+console.log('Is personB.Age of Driving Age?' + personB.Age > drivingage);
 
 
 
@@ -19,24 +64,28 @@
 // ****************************************************TASK 2 - MATHS OPERATORS****************************************************
 // ***Uncomment the code below to begin the task***
 
-// const x = 6;
-// const y = 4;
+const x = 6;
+const y = 4;
 
-// const addition = x + y;
+const addition = x + y;
 
-// console.log("Addition: x + y " + addition);
+console.log("Addition: x + y " + addition);
 
-// const subtraction = x - y;
+const subtraction = x - y;
 
-// console.log("Subtraction: x - y " + subtraction);
+console.log("Subtraction: x - y " + subtraction);
 
-// const multiplication = x * y;
+const multiplication = x * y;
 
-// console.log("multiplication: x * y " + multiplication);
+console.log("multiplication: x * y " + multiplication);
 
-// const division = x * y;
+const division = x / y;
 
-// console.log("Division: x + y " + division);
+console.log("Division: x / y " + division);
+
+const additionand = x + y - y;
+
+console.log("additionand: x + y - y" + additionand);
 
 
 
--- session-6/index.js
+++ session-6/index.js
@@ -1,6 +1,12 @@
 // Use this randomNumber inside of the while/for loops to generate a random number
+let counter = 1
 const randomNumber = Math.round(Math.random() * 10);
+while (counter <= 10) {
+   // randomNumber = counter;
+counter = counter + 1;
+}
 
+console.log ('random number: ' + randomNumber);
 // ****************************************************TASK 1****************************************************
 
 
