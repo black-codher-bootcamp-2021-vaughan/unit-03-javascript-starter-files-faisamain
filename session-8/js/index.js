32548fe70db016b7e9cf639a73cf5f1311d27f02
faisamain <faisamain@hotmail.com>
2021-12-15 20:36:17 +0000

    session 8 tasks

--- session-8/js/index.js
+++ session-8/js/index.js
@@ -1,3 +1,57 @@
+
+
+
+//let btn =document.createElement ("button");
+//btn.innerHTML = "displayPopulation";
+//document.body.appendChild(btn);
+//button.onclick = displayPopulation
+
 import displayPopulation from "./birmingham.js";
 import { fruits } from "./fruit.js";
 import createListOfFruits from "./giveMeFruits.js";
+
+const button = document.createElement('button')
+const text = document.createTextNode('Show Population')
+button.appendChild(text);
+button.onclick = displayPopulation;
+document.body.appendChild(button);
+
+//createListOfFruits(fruits);
+
+const x = document.querySelector("#clickMe");
+const y = document.querySelector("#hovering");
+
+x.addEventListener("click", respondClick);
+y.addEventListener("mouseover", respondMouseOver);
+y.addEventListener("mouseout", respondMouseOut);
+
+function respondMouseOver() {
+  document.querySelector("#effect").innerHTML += "MouseOver Event" + "<br>";
+}
+
+function respondMouseOut() {
+  document.querySelector("#effect").innerHTML += "MouseOut Event" + "<br>";
+}
+
+function respondClick() {
+  document.querySelector("#effect").innerHTML += "Click Event" + "<br>";
+}
+
+
+// remove part
+
+document
+  .querySelector("#remove-handler")
+  .addEventListener("mousemove", randomNumber);
+
+function randomNumber() {
+  document.querySelector("#random-number").innerHTML = Math.random();
+}
+
+document
+  .querySelector("#remove-handler")
+  .addEventListener("click", removeEvent );
+
+function removeEvent () {
+    document.querySelector("#remove-handler").removeEventListener ("mousemove", randomNumber)
+}
